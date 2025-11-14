3e8f9133d9bccf89011c038a5c1cb2a9e6010f7c
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-11-14 22:51:23 +0500

    Task1

---
+++ Tyuiu.TomilovAD.Sprint4.Task1.V20.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TomilovAD.Sprint4.Task1.V20.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint4.Task1.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 8, 0, 4, 7, 5, 3, 9, 2, 7, 0 };
+            int wait = 31;
+            int res = ds.Calculate(array);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint4.Task1.V20/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.TomilovAD.Sprint4.Task1.V20.Lib;
+namespace Tyuiu.TomilovAD.Sprint4.Task1.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил Томилов А. Д. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] numsarray = new int[10];
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Введите " + i + " элемент массива");
+                numsarray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int a = ds.Calculate(numsarray);
+
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+    }
+}
