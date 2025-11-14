cdc997bc24ab280189cf7059244c66fb56968a18
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-11-14 22:09:55 +0500

    Add project files.

---
+++ Tyuiu.TomilovAD.Sprint4.Task0.V14.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.TomilovAD.Sprint4.Task0.V14.Lib
+{
+    public class DataService : ISprint4Task0V14
+    {
+        public int GetSumOddArrEl(int[] array)
+        {
+            int o = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    o += array[i];
+                }
+            }
+            return o;
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint4.Task0.V14/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.TomilovAD.Sprint4.Task0.V14.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint4.Task0.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил Томилов А. Д. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Массив {8, 9, 5, 4, 3, 2, 1, 0, 5, 7}                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int[] array = { 8, 9, 5, 4, 3, 2, 1, 0, 5, 7 };
+
+            Console.WriteLine(ds.GetSumOddArrEl(array));
+
+            Console.ReadLine();
+        }
+    }
+}
