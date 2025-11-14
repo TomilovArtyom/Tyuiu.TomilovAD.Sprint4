2041a7a5b227ac2d3100134e64393b5407ccf562
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-11-15 00:39:41 +0500

    Task3

---
+++ Tyuiu.TomilovAD.Sprint4.Task3.V16.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.TomilovAD.Sprint4.Task3.V16.Lib
+{
+    public class DataService : ISprint4Task3V16
+    {
+        public int Calculate(int[,] array)
+        {
+
+
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+
+            int a = 10;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int j = columns - 1;
+                if (array[i, j] < a)
+                {
+                    a = array[i, j];
+                }                   
+            }    
+            return a;
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint4.Task3.V16/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.TomilovAD.Sprint4.Task3.V16.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint4.Task3.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил Томилов А. Д. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (статический ввод)                              *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int[,] a = new int[5, 5] { { 5, 8, 5, 8, 4 }, { 2, 3, 4, 6, 3 }, { 1, 1, 2, 9, 9 }, { 6, 7, 4, 1, 2 }, { 5, 7, 1, 8, 7 } };
+            Console.WriteLine(ds.Calculate(a));
+        }
+    }
+}
