5a3f4871ea28ef61bdf6df46673de9a58cb937a5
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-11-14 23:27:32 +0500

    Task2

---
+++ Tyuiu.TomilovAD.Sprint4.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TomilovAD.Sprint4.Task2.V11.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint4.Task2.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 8, 0, 4, 7, 5, 3, 9, 2, 7, 0 };
+            int wait = 14;
+            int res = ds.Calculate(array);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint4.Task2.V11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using Tyuiu.TomilovAD.Sprint4.Task2.V11.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint4.Task2.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+
+            Console.Title = "Спринт #4 | Выполнил Томилов А. Д. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] numsarray = new int[15];
+
+            for (int i = 1; i <= 10; i++)
+            {
+                numsarray[i] = rnd.Next(3, 8);
+            }
+
+            Console.WriteLine(ds.Calculate(numsarray));
+            Console.ReadKey();
+        }
+    }
+}
