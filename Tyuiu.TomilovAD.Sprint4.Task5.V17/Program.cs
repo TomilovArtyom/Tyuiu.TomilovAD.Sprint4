d71727359f764b556ee52e2b980dfde31d2386c9
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-11-15 01:31:10 +0500

    Task5

--- Tyuiu.TomilovAD.Sprint4.Task5.V17/Program.cs
+++ Tyuiu.TomilovAD.Sprint4.Task5.V17/Program.cs
@@ -1,30 +1,29 @@
-﻿using Tyuiu.TomilovAD.Sprint4.Task4.V7.Lib;
-namespace Tyuiu.TomilovAD.Sprint4.Task4.V7
+﻿using Tyuiu.TomilovAD.Sprint4.Task5.V17.Lib;
+namespace Tyuiu.TomilovAD.Sprint4.Task5.V17
 {
     class Program
     {
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-
+            Random rnd = new Random();
             Console.Title = "Спринт #4 | Выполнил Томилов А. Д. | ИСТНб-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #4                                                               *");
-            Console.WriteLine("* Тема: Двумерные массивы (ввод с клавиатуры)                             *");
-            Console.WriteLine("* Задание #4                                                              *");
-            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Тема: Двумерные массивы (генератор случайных чисел)                     *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
             Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            int[,] numsarray = new int[5,5];
+            int[,] numsarray = new int[5, 5];
 
             for (int i = 1; i <= 5; i++)
             {
                 for (int j = 1; j <= 5; j++)
                 {
-                    Console.WriteLine("Введите " + i + " элемент массива строки" +j);
-                    numsarray[i, j] = Convert.ToInt32(Console.ReadLine());
+                    numsarray[i, j] = rnd.Next(-9,4);
                 }
             }
             Console.WriteLine(ds.Calculate(numsarray));
